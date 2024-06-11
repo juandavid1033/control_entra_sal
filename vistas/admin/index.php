4bef4283afdbab9dbb671a2a55d4d134c2806e4e
Yesica123lorn <137794378+Yesica123lorn@users.noreply.github.com>
2024-06-11 05:30:11 -0500

    Update index.php

--- vistas/admin/index.php
+++ vistas/admin/index.php
@@ -116,6 +116,10 @@ $fila = $info->fetch();
                         <h6 class="collapse-header">Visitante</h6>
                         <a class="collapse-item" href="./tablas/visitante.php">Informes de Visitantes</a>
                     </div>
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Vigilante</h6>
+                        <a class="collapse-item" href="./tablas/vigilante.php">Informes de Vigilante</a>
+                    </div>
                 </div>
             </li>
             <!-- Divider -->
@@ -200,121 +204,25 @@ $fila = $info->fetch();
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">BIENVENIDO</h1>
-                    </div>
-
+                  
                     <!-- Content Row -->
-                    <div class="row">
-
-                        <!-- PRIMERA CARTA DE LOS PRECIOS -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Visitantes(Por Dia)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">400</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                   
 
                         <!-- SEGUNDA -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Registrados en el sistema</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        
 
                         <!-- TERCERA -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-info shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                Asitencia(Por Dia)
-                                            </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">90%</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 90%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                      
                         </div>
 
                         <!-- CUARTA -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Vehículos Registrados</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">180</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                       
 
                     <!-- Content Row -->
 
 
                     <!-- Content Row -->
-                    <div class="row">
-
-                        <!-- Content Column -->
-                        <div class="col-lg-6 mb-4">
-                            <a href="respaldo/respaldo.php"
-                                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                    class="fas fa-download fa-sm text-white-50"></i>Descargar Base De Datos</a>
-                        </div>
-                        <a href="respaldo/importar.php"
-                            class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="bi bi-database-up"></i> Subir la Base De Datos</a>
-
-                        <div class="col-lg-6 mb-4">
-
-
-
-                        </div>
-                    </div>
-
+                    
                 </div>
                 <!-- /.container-fluid -->
 
@@ -322,13 +230,7 @@ $fila = $info->fetch();
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; 2024</span>
-                    </div>
-                </div>
-            </footer>
+           
             <!-- End of Footer -->
 
         </div>
