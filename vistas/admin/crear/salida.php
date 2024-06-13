d3b7f3924308ebbed86ed382b4a31e8b563f4898
Yesica123lorn <137794378+Yesica123lorn@users.noreply.github.com>
2024-06-13 06:19:35 -0500

    Update salida.php

--- vistas/admin/crear/salida.php
+++ vistas/admin/crear/salida.php
@@ -14,7 +14,7 @@ $empieza = ($pagina - 1) * $por_pagina;
 $sql1 = $conex->prepare("SELECT * FROM entrada_salidas 
     LEFT JOIN usuario ON entrada_salidas.documento = usuario.documento 
     LEFT JOIN tipo_entrada ON entrada_salidas.tipo_entrada = tipo_entrada.id_tipo_entrada 
-    WHERE entrada_salidas.estado = 2
+    WHERE entrada_salidas.estado = 1
     ORDER BY entrada_salidas.documento 
     LIMIT :empieza, :por_pagina");
 
@@ -23,15 +23,10 @@ $sql1->bindParam(':por_pagina', $por_pagina, PDO::PARAM_INT);
 $sql1->execute();
 $resultado1 = $sql1->fetchAll(PDO::FETCH_ASSOC);
 
-$sql = $conex->prepare("SELECT COUNT(*) FROM entrada_salidas WHERE estado = 2");
+$sql = $conex->prepare("SELECT COUNT(*) FROM entrada_salidas WHERE estado = 1");
 $sql->execute();
 $resul = $sql->fetchColumn();
 $total_paginas = ceil($resul / $por_pagina);
-if ($total_paginas == 0) {
-    echo "<center>" . 'Lista Vacia' . "</center>";
-} else {
-    echo "<center><a href='t_cliente.php?pagina=1'>" . "<i class='fa fa-arrow-left'></i>" . "</a>";
-}
 ?>
 
 <!DOCTYPE html>
@@ -44,26 +39,26 @@ if ($total_paginas == 0) {
     <link rel="stylesheet" href="../../../css/stiledi.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-    <title>Lista de salida</title>
+    <title>Salida</title>
     <link href="../../../img1/logo9.png" rel="icon">
 </head>
 
 <body>
 
-    <a class="btn btn-success" href="../index.php" style="margin-left: -45%; margin-top:3%; position:absolute;">
+    <a class="btn btn-success" href="../index.php" style="margin-top:3%; position:absolute;">
         <i class="bi bi-chevron-left" style="padding:10px 14px 10px 10px; color:#fff; font-size:15px; background-color:#0d6efd; border-radius:10px;">
             REGRESAR
         </i>
     </a>
 
     <br>
-    <a class="btn btn-success" href="excelusu.php" style="margin-left: 90%; margin-top:0%;">
+    <a class="btn btn-success" href="../excel/excel_clientes.php" style="margin-left: 90%; margin-top:0%;">
         <i class="bi bi-file-earmark-excel" style="padding:10px 10px 10px 10px; border-radius:10px; color:#fff; font-size:15px; background-color:#198754;">
             EXCEL
         </i>
     </a>
     <br>
-    <a class="btn btn-success" href="reporte_usu.php" style="margin-left: 88%; margin-top:1%;">
+    <a class="btn btn-success" href="../reporte/repor_cliente.php" style="margin-left: 88%; margin-top:1%;">
         <i class="bi bi-printer" style="padding:10px 16px 10px 16px; border-radius:10px; color:#fff; font-size:15px; background-color:#E00000;">
             IMPRIMIR
         </i>
@@ -82,6 +77,7 @@ if ($total_paginas == 0) {
                 <th>Fecha y Hora de Salida</th>
                 <th>Placa</th>
                 <th>Serial</th>
+                <th>Accion</th>
             </tr>
         </thead>
 
@@ -94,10 +90,17 @@ if ($total_paginas == 0) {
                 <td><?= !empty($usu['salida_fecha_hora']) ? date("Y-m-d H:i:s", strtotime($usu['salida_fecha_hora'])) : 'No registrado' ?></td>
                 <td><?= !empty($usu['id_placa']) ? $usu['id_placa'] : 'No' ?></td>
                 <td><?= !empty($usu['serial']) ? $usu['serial'] : 'No' ?></td>
+                <td>
+                    <form method="post" action="../tablas/estado/estado.php">
+                        <input type="hidden" name="estado" value="<?= $usu['id_entrada_salida'] ?>">
+                        <button type="submit">Salida</button>
+                    </form>
+                </td>
             </tr>
         <?php } ?>
+
     </table>
-    <div class="text-center" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="text-center" style="margin-top: 20px;" role="toolbar" aria-label="Toolbar with button groups">
         <div class="btn-group me-2" role="group" aria-label="First group">
             <?php for ($i = 1; $i <= $total_paginas; $i++) {
                 echo "<a class='btn btn-primary' href='t_cliente.php?pagina=" . $i . "'> " . $i . " </a>";
